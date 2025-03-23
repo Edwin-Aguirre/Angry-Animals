c0264aff4d2c936d2ae3470ece060c77367c983c
Edwin-Aguirre <edwina3@uci.edu>
2025-03-23 15:46:12 -0700

    Project Completed

--- scripts/cup.gd
+++ scripts/cup.gd
@@ -17,6 +17,10 @@ func die() -> void:
 	animation_player.play("vanish")
 	
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("exit") and num_cups > 0:
+		num_cups = 0
+
 
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	num_cups -= 1
--- scripts/level_button.gd
+++ scripts/level_button.gd
@@ -6,12 +6,33 @@ extends TextureButton
 
 @onready var level_label: Label = $MarginContainer/VBoxContainer/LevelLabel
 @onready var score_label: Label = $MarginContainer/VBoxContainer/ScoreLabel
+@onready var star: Sprite2D = $MarginContainer/VBoxContainer/Star
+
+
+const STAR_BRONZE = preload("res://assets/other/starBronze.png")
+const STAR_DIAMOND = preload("res://assets/other/starDiamond.png")
+const STAR_GOLD = preload("res://assets/other/starGold.png")
+const STAR_SILVER = preload("res://assets/other/starSilver.png")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	level_label.text = level_number
 	score_label.text = str(ScoreManager.get_level_best(level_number))
+	score_star()
+
+
+func score_star() -> void:
+	var best_score = ScoreManager.get_level_best(level_number)
+	
+	if best_score <= 1:
+		star.texture = STAR_DIAMOND
+	elif best_score <= 5:
+		star.texture = STAR_GOLD
+	elif best_score <= 10:
+		star.texture = STAR_SILVER
+	elif best_score <= 20:
+		star.texture = STAR_BRONZE
 
 
 func _on_mouse_entered() -> void:
